0bdf45f24a4737f2c0e7e9fc34b814ea0a1826cb
Faraz Hussain <aigeniusfaraz@gmail.com>
2025-11-12 07:46:53 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-
-# Set dark theme for chart
-plt.style.use('dark_background')
+try:
+    import matplotlib.pyplot as plt
+    # Set dark theme for chart
+    plt.style.use('dark_background')
+except ImportError:
+    st.error("Matplotlib not installed. Please check requirements.txt")
+    st.stop()
 
 # Page config
 st.set_page_config(page_title="Student Marks Analyzer", layout="centered")
